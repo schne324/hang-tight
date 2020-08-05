d81d501bcc296a2b6eb5d18dda00e4309be4c635
Harris Schneiderman <schne324@gmail.com>
2020-08-04 23:25:48 -0700

    Updates docs with code examples (#8)

--- packages/docs/components/App.tsx
+++ packages/docs/components/App.tsx
@@ -8,7 +8,12 @@ import {
   Squares,
   Waves,
 } from 'hang-tight-react';
+import Refractor from 'react-refractor';
+import powershell from 'refractor/lang/powershell';
+import Section from './Section';
 import './App.css';
+
+Refractor.registerLanguage(powershell);
 
 const App: React.ComponentType = () => {
   const [animationEnabled, setAnimationEnabled] = useState(true);
@@ -18,43 +23,41 @@ const App: React.ComponentType = () => {
       <h1>
         <span>Hang</span> <span>tight</span>
       </h1>
+      <h2>Accessible, pure css loaders.</h2>
       <p>
         <a href="https://github.com/schne324/hang-tight">view on github</a>
       </p>
-      <code>npm install --save hang-tight hang-tight-react</code>
+      <Refractor
+        className="Install"
+        language="powershell"
+        value="npm install --save hang-tight hang-tight-react"
+      />
       <label>
         <span>Enable amimation/motion</span>
         <input checked={animationEnabled} type="checkbox" onChange={onChange} />
       </label>
       <div className="grid">
-        <section>
-          <h2>Dots</h2>
+        <Section name="Dots">
           <Dots paused={!animationEnabled} />
-        </section>
-        <section className="min">
-          <h2>Squares</h2>
+        </Section>
+        <Section name="Squares" className="min">
           <Squares paused={!animationEnabled} />
-        </section>
-        <section>
-          <h2>Spinning Circles</h2>
+        </Section>
+        <Section name="SpinningCircles">
           <SpinningCircles paused={!animationEnabled} />
-        </section>
-        <section>
-          <h2>Ripple</h2>
+        </Section>
+        <Section name="Ripple">
           <Ripple paused={!animationEnabled} />
-        </section>
-        <section className="min">
-          <h2>EQ</h2>
+        </Section>
+        <Section name="EQ" className="EQ min">
           <EQ paused={!animationEnabled} />
-        </section>
-        <section>
-          <h2>SlidingBars</h2>
+        </Section>
+        <Section name="SlidingBars">
           <SlidingBars paused={!animationEnabled} />
-        </section>
-        <section>
-          <h2>Waves</h2>
+        </Section>
+        <Section name="Waves">
           <Waves paused={!animationEnabled} />
-        </section>
+        </Section>
       </div>
     </main>
   );
